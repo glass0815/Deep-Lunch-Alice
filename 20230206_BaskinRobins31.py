1bed0cc8d9dd2590a38f8aefa2eb076a3b8ddba3
glass0815 <60865525+glass0815@users.noreply.github.com>
2023-02-03 04:35:21 +0000

    최초 커밋

---
+++ 20230206_BaskinRobins31.py
@@ -0,0 +1,8 @@
+def run():
+
+	return
+
+
+if __name__ == '__main__':
+	print('>> BaskinRobins31 Start!')
+	run()
---
+++ 20230206_UpDownGame.py
@@ -0,0 +1,8 @@
+def run():
+
+	return
+
+
+if __name__ == '__main__':
+	print('>> Up & Down Game Start!')
+	run()
